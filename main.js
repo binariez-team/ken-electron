21ad1df7db427c811ad763211e1adbb31e82988b
JaafarMortada <mortada9190@gmail.com>
2024-05-29 23:57:48 +0300

    updates

--- main.js
+++ main.js
@@ -4,38 +4,38 @@ const {
     ipcMain,
     dialog,
     Menu,
-    shell
-} = require('electron')
-
+    shell,
+} = require("electron");
 
 // Menu
-const template = require('./menu');
-const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
+const template = require("./menu");
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
-const path = require('path')
+const path = require("path");
 
 // electron context menu
-contextMenu = require('electron-context-menu');
+contextMenu = require("electron-context-menu");
 contextMenu({
     showSaveImageAs: false,
     showSearchWithGoogle: false,
     showInspectElement: false,
     showSelectAll: false,
-    showCopyImage: false
+    showCopyImage: false,
 });
-
 
 // Check if electron is in development mode to enable Node.js on release mode
 var node;
-const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
+const isEnvSet = "ELECTRON_IS_DEV" in process.env;
 const getFromEnv = Number.parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
 const isDev = isEnvSet ? getFromEnv : !app.isPackaged;
 if (!isDev) {
     // require server
-    const server = require('../server');
-    node = server.listen(3500, () => console.log(`listening on port ${3500} ...`));
-} 
+    const server = require("../server");
+    node = server.listen(3500, () =>
+        console.log(`listening on port ${3500} ...`)
+    );
+}
 
 async function createWindow() {
     const win = new BrowserWindow({
@@ -43,69 +43,93 @@ async function createWindow() {
         height: 600,
         show: false,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-    win.maximize()
-    win.show()
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+    win.maximize();
+    win.show();
 
     const loadSystem = async function () {
         if (isDev) {
-            win.loadURL('http://localhost:4200')
+            win.loadURL("http://localhost:4200");
         } else {
-            win.loadFile('app/index.html')
+            win.loadFile("app/index.html");
         }
-    }
+    };
 
-    loadSystem()
+    loadSystem();
 
-    win.webContents.on('did-fail-load', () => loadSystem());
+    win.webContents.on("did-fail-load", () => loadSystem());
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
 
-    app.on('activate', () => {
+    app.on("activate", () => {
         if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
+            createWindow();
         }
-    })
-})
+    });
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         if (!isDev) {
             node.close();
         }
-        app.quit()
+        app.quit();
     }
-})
-
+});
 
 let printWindow;
-ipcMain.handle('print-invoice', async (event, data) => {
+ipcMain.handle("print-invoice", async (event, data) => {
     // console.log(data);
     printWindow = new BrowserWindow({
         width: 706.95553,
         height: 1000,
         show: false,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
 
-    printWindow.loadFile('assets/print.html');
+    printWindow.loadFile("assets/print.html");
     printWindow.show();
 
     const printOptions = {
         silent: false, // Print without showing a dialog (optional)
         marginsType: 0, // Set margin type (optional)
-        // Other options as per documentation
-      };
-    printWindow.webContents.on('did-finish-load', async function () {
-        await printWindow.webContents.send('printDocument', data);
+    };
+    printWindow.webContents.on("did-finish-load", async function () {
+        await printWindow.webContents.send("printDocument", data);
         printWindow.webContents.print(printOptions, (success) => {
             printWindow.close();
         });
-    })
-})
+    });
+});
+
+ipcMain.handle("print-statement", async (event, data) => {
+    // console.log(data);
+    printWindow = new BrowserWindow({
+        width: 706.95553,
+        height: 1000,
+        show: false,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    printWindow.loadFile("assets/printStatement.html");
+    printWindow.show();
+
+    const printOptions = {
+        silent: false, // Print without showing a dialog (optional)
+        marginsType: 0, // Set margin type (optional)
+    };
+    printWindow.webContents.on("did-finish-load", async function () {
+        await printWindow.webContents.send("printDocument", data);
+        printWindow.webContents.print(printOptions, (success) => {
+            printWindow.close();
+        });
+    });
+});
