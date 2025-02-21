eb5957f9eb98e5c8346baf3015bda8a3eb0385a1
Jaafar Mortada <mortada9190@gmail.com>
2025-02-21 17:08:17 +0200

    updates

--- main.js
+++ main.js
@@ -115,7 +115,7 @@ ipcMain.handle("print-invoice", async (event, data) => {
     printWindow.webContents.on("did-finish-load", async function () {
         await printWindow.webContents.send("printDocument", data);
         printWindow.webContents.print(printOptions, (success) => {
-            printWindow.close();
+            // printWindow.close();
         });
     });
 });
